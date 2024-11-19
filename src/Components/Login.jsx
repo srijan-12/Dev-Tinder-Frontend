9846cbb4ddf53cb1a5aa8baae4e73dfc24a24453
Srijan Sinha <srijansinha@Srijans-MacBook-Air.local>
2024-11-20 00:52:58 +0530

    logout with error handling done

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -8,14 +8,16 @@ export const Login = () =>{
     const dispatch = useDispatch();
     const[email,setEmail] = useState("amitsaha@gmail.com");
     const[password, setPassword] = useState("k8dfh8c@Pfv0gB2");
+    const [error, setError] = useState(null);
     const navigator = useNavigate();
     const handleLogin = async () =>{
         try{
             const res = await axios.post(`${backendBaseURL}/login`, {email,password},{withCredentials:true});
+            console.log(res)
             dispatch(addUser(res.data.user));
             navigator("/");
         }catch(err){
-            console.log(err.message)
+            setError(err.response.data.error);
             navigator("/login");
         }
     }
@@ -29,7 +31,8 @@ export const Login = () =>{
                                 <input id="username" placeholder="e-mail" className="p-3 rounded-xl w-10/12 m-4" value={email} onChange={(e)=> setEmail(e.target.value)}/>
                                 <input id="password" placeholder="password" className="p-3 rounded-xl w-10/12 m-4" value={password} onChange={(e)=> setPassword(e.target.value)}/>
                             </div>
-                            <div className="card-actions justify-start ms-5 ">
+                            <div className="card-actions justify-start ms-5 flex flex-col">
+                                <p className="text-red-500">{error}</p>
                                 <button className="btn btn-primary w-3/12 mt-2" onClick={handleLogin}>login</button>
                             </div>
                     </div>
